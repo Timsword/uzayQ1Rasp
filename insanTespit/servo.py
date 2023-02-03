c1b7af2e2e9d9f879aaad3a94cd528ee32bfd8ce
kagancbn <kagancoban92@gmail.com>
2023-02-03 16:46:36 +0300

    uzayktu

---
+++ insanTespit/servo.py
@@ -0,0 +1,26 @@
+# Import libraries
+import RPi.GPIO as GPIO
+import time
+
+def servoCalistir(angle):
+    # Set GPIO numbering mode
+    GPIO.setmode(GPIO.BOARD)
+
+    # Set pin 11 as an output, and set servo1 as pin 11 as PWM
+    GPIO.setup(11,GPIO.OUT)
+    servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
+
+    #start PWM running, but with value of 0 (pulse off)
+    servo1.start(0)
+    print ("Waiting for 2 seconds")
+    time.sleep(2)
+
+    duty = angle / 18 + 2
+    GPIO.output(11, True)
+    servo1.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(11, False)
+    servo1.ChangeDutyCycle(0)
+
+    servo1.stop()
+    GPIO.cleanup()
